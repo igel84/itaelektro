33d36c6e4d524565e4c4422f55d6e26707636307
igel84 <igel.yulian@mail.ru>
2012-11-29 02:02:36 +0400

    fin_filt

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,19 @@ class ProductsController < ApplicationController
   end
 
   def find_products
-    @products = Kaminari.paginate_array(Product.find(:all, :joins => :catalogue_section, :conditions => ['products.name like ? OR products.kod like ? OR catalogue_sections.name like ?', '%'+params[:query]+'%', '%'+params[:query]+'%', '%'+params[:query]+'%'])).page params[:page]
+    if params[:query]
+      @products = Kaminari.paginate_array(Product.find(:all, :joins => :catalogue_section, :conditions => ['products.name like ? OR products.kod like ? OR catalogue_sections.name like ?', '%'+params[:query]+'%', '%'+params[:query]+'%', '%'+params[:query]+'%'])).page params[:page]
+    else
+      @products = [] 
+      @products = Product.where('name like ?', '%'+params[:name]+'%') if params[:name] && !params[:name].blank?
+      @products |= Product.where('brand like ?', '%'+params[:brand]+'%') if params[:brand] && !params[:brand].blank?
+      @products |= Product.where('kod like ?', '%'+params[:kod]+'%') if params[:kod] && !params[:kod].blank?
+
+      @products |= Product.where('price > ? AND price < ?', params[:price_min], params[:price_max]) if params[:kod] && !params[:kod].blank?
+
+      @products |= Product.where(Product.find(:all, :joins => :catalogue_section, :conditions => ['catalogue_sections.name like ?', '%'+params[:catalogue_sections]+'%'])) if params[:catalogue_sections] && !params[:catalogue_sections].blank?
+
+      @products = Kaminari.paginate_array(@products).page params[:page]
+    end        
   end
 end
